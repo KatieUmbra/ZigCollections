483b0f72889312fa810fe9e32757d8665a718b19
106122241+KatieUmbra@users.noreply.github.com <Katherine@kaytea.dev>
2023-11-06 03:32:10 -0600

    wip: [DynamicArray] testing

--- src/dynamic_array.zig
+++ src/dynamic_array.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 
 const Allocator = std.mem.Allocator;
 
+/// [Dynamic Array] or [Vector] Struct. Is an array whose size can change at runtime, allows for insertions, removals and other related operations.
+/// It's default growth is geometric (b * 2^n)
 pub fn DynamicArray(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -14,6 +16,7 @@ pub fn DynamicArray(comptime T: type) type {
         /// This field is for internal use of DynamicArray only
         _allocator: Allocator = undefined,
 
+        /// Initializes allocator, acompany with `defer DynamicArray(T).deinit()`
         pub fn init(self: *Self, allocator: Allocator) !void {
             self._size = 0;
             self._allocator = allocator;
@@ -23,23 +26,18 @@ pub fn DynamicArray(comptime T: type) type {
             errdefer self._allocator.free(self._data);
         }
 
-        pub fn deinit(self: *Self) !void {
-            self._data = self._allocator.free(self._data);
+        /// Frees vector data
+        pub fn deinit(self: *Self) void {
+            self._allocator.free(self._data);
         }
 
-        pub fn set(self: *Self, element: T, index: usize) !void {
-            if (self._limit == index) {
-                try self.pushBack(element);
-            } else {
-                self._data[index] = element;
-            }
-        }
-
+        /// Pushes data at index
         pub fn push(self: *Self, element: T, index: usize) !void {
             if (index < self._size) {
                 const values = self._data[index..self._size];
                 var values_copy = try self._allocator.dupe(T, values);
-                self.erase(values_copy.len);
+                defer self._allocator.free(values_copy);
+                self.eraseBack(values_copy.len);
                 try self.pushBack(element);
                 for (values_copy) |value| {
                     try self.pushBack(value);
@@ -51,10 +49,12 @@ pub fn DynamicArray(comptime T: type) type {
             }
         }
 
+        /// Pushes at the start of the vector
         pub fn pushFront(self: *Self, element: T) !void {
             try self.push(element, 0);
         }
 
+        /// Pushes at the end of the vector
         pub fn pushBack(self: *Self, element: T) !void {
             if (self._size == self._limit) {
                 self._limit *= 2;
@@ -64,25 +64,126 @@ pub fn DynamicArray(comptime T: type) type {
             self._size += 1;
         }
 
+        /// Removes and returns element at index
+        pub fn pop(self: *Self, index: usize) !T {
+            if (index == self._size) {
+                return self.popLast();
+            } else if (index < self._size) {
+                const slice = self._data[index + 1 .. self._size];
+                const values = try self._allocator.dupe(T, slice);
+                const returned = try self._allocator.dupe(T, self._data[index .. index + 1]);
+                defer self._allocator.free(values);
+                defer self._allocator.free(returned);
+                self.eraseBack(values.len + 1);
+                for (values) |value| {
+                    try self.pushBack(value);
+                }
+                self._size -= 1;
+                return returned[0];
+            } else {
+                return error.BadInput;
+            }
+        }
+
+        /// Removes and returns last element
+        pub fn popBack(self: *Self) T {
+            return self.pop(self._size);
+        }
+
+        /// Removes and returns first element
+        pub fn popFront(self: *Self) T {
+            return self.pop(0);
+        }
+
+        /// Removes element at index
+        pub fn delete(self: *Self, index: usize) !void {
+            _ = try self.pop(index);
+        }
+
+        /// Removes N elements from the back
+        pub fn eraseBack(self: *Self, amount: usize) void {
+            self._size -= amount;
+        }
+
+        /// Removes N elements from the front
+        pub fn eraseFront(self: *Self, amount: usize) void {
+            var i: u32 = 0;
+            while (i < amount) {
+                self.delete(0);
+                i += 1;
+            }
+        }
+
+        /// Resizes vector to be of size N exactly, shrinking or growing if necessary
+        pub fn resize(self: *Self, _size: usize) !void {
+            const resized = self._allocator.resize(self._data, _size);
+            if (!resized) {
+                return error.FailedResize;
+            }
+            self._limit = _size;
+            if (self._size > _size) {
+                self._size = _size;
+            }
+        }
+
+        /// Reserves N extra spaces exactly
+        pub fn reserve(self: *Self, _size: usize) !void {
+            const newLimit = self._size + _size;
+            const resized = self._allocator.resize(self._data, newLimit);
+            if (!resized) {
+                return error.FailedResize;
+            }
+            if (self._limit < newLimit) {
+                self._limit = newLimit;
+            }
+        }
+
+        /// Reserves N extra spaces geometrically
+        pub fn expand(self: *Self, _size: usize) !void {
+            var togo = _size;
+            var newLimit = self._limit;
+
+            while (togo > 0) {
+                if (newLimit - self._size >= togo) {
+                    togo = 0;
+                } else {
+                    togo -= newLimit;
+                    newLimit *= 2;
+                }
+            }
+
+            const resized = self._allocator.resize(self._data, self._limit);
+            if (!resized) {
+                return error.FailedResize;
+            }
+            self._limit = newLimit;
+        }
+
+        /// Returns the size of the vector
         pub fn size(self: *Self) usize {
             return self._size;
         }
 
-        pub fn pop(self: *Self) T {
-            const last = self._data[self._size];
-            self._size -= 1;
-            return last;
+        /// Returns the current capacity of the vector
+        pub fn capacity(self: *Self) usize {
+            return self._limit;
         }
 
-        pub fn erase(self: *Self, amount: usize) void {
-            self._size -= amount;
-        }
-
+        /// Gets element at index N
         pub fn get(self: *Self, index: usize) ?T {
             if (index <= self._size) {
                 return self._data[index];
             }
             return null;
+        }
+
+        /// Replaces the data at index with the provided data
+        pub fn set(self: *Self, element: T, index: usize) !void {
+            if (self._limit == index) {
+                try self.pushBack(element);
+            } else {
+                self._data[index] = element;
+            }
         }
     };
 }
--- src/main.zig
+++ src/main.zig
@@ -4,10 +4,10 @@ const testing = std.testing;
 const DynamicArray = @import("dynamic_array.zig").DynamicArray;
 
 test "dynamic array pushing" {
-    var allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer allocator.deinit();
+    var allocator = std.testing.allocator;
     var myVector = DynamicArray(i32){};
-    try myVector.init(allocator.allocator());
+    try myVector.init(allocator);
+    defer myVector.deinit();
     try myVector.pushBack(16);
     try myVector.pushBack(15);
     try myVector.pushBack(14);
@@ -19,35 +19,37 @@ test "dynamic array pushing" {
 }
 
 test "dynamic array insert" {
-    var allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer allocator.deinit();
+    var allocator = std.testing.allocator;
     var myVector = DynamicArray(i32){};
-    try myVector.init(allocator.allocator());
+    try myVector.init(allocator);
+    defer myVector.deinit();
     try myVector.pushBack(20);
     try myVector.pushBack(21);
     try myVector.pushFront(1);
-    std.debug.print("\n", .{});
-    var i: u32 = 0;
-    while (i < myVector.size()) {
-        const element: i16 = @truncate(myVector.get(i).?);
-        std.log.warn("element: {any}\n", .{element});
-        i += 1;
-    }
-    std.debug.print("\n", .{});
     if (myVector.get(2)) |value| {
-        const size: u16 = @truncate(myVector.size());
-        const val: i16 = @truncate(value);
-        std.log.warn("value: {any}\n", .{val});
-        std.log.warn("size: {any}\n", .{size});
         try testing.expect(value == 21);
     }
 }
 
-test "dynamic array getting" {
-    var allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer allocator.deinit();
+test "dynamic array popping" {
+    var allocator = std.testing.allocator;
     var myVector = DynamicArray(i32){};
-    try myVector.init(allocator.allocator());
+    try myVector.init(allocator);
+    defer myVector.deinit();
+    var i: i32 = 0;
+    while (i < 200) {
+        try myVector.pushBack(i);
+        i += 1;
+    }
+    _ = try myVector.pop(100);
+    try testing.expect(myVector.popLast() == 199);
+}
+
+test "dynamic array getting" {
+    var allocator = std.testing.allocator;
+    var myVector = DynamicArray(i32){};
+    try myVector.init(allocator);
+    defer myVector.deinit();
     try myVector.pushBack(16);
     try myVector.pushBack(15);
     try myVector.pushBack(14);
